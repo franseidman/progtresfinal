271bcea0754ea1ab03acf86a07e126d2f321fd5c
lruizcoines <lruizcoines@gmail.com>
2021-10-21 09:39:42 -0300

    menu

--- progtresfinal/App.js
+++ progtresfinal/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import Login from './src/screens/Login';
-import Register from './src/screens/Register';
-import Home from './src/screens/Home';
+import Menu from './src/screens/Menu';
 
 export default function App() {
 
-  const Drawer = createDrawerNavigator();
-
   return ( 
-      <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Login">
-          <Drawer.Screen name = "Login" component={Login}></Drawer.Screen>
-          <Drawer.Screen name = "Registro" component={Register}></Drawer.Screen>
-          <Drawer.Screen name = "Home" component={Home}></Drawer.Screen>
-        </Drawer.Navigator>
-      </NavigationContainer>
+      <Menu/>
   );
 }
-
